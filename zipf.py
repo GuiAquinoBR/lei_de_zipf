f3980f7777417a06bd856405e9af409e86d087eb
Guilherme Aquino <59674194+GuiAquinoBR@users.noreply.github.com>
2022-08-17 01:18:25 -0300

    Update zipf.py

--- zipf.py
+++ zipf.py
@@ -1,6 +1,12 @@
+# Para o dataframe
 import pandas as pd
+
+# Para o gráfico
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# Para stopwords
+import nltk
 
 # Extraindo e Limpando os Dados:
 
@@ -17,24 +23,26 @@ for i in range(len(texto)):
     texto[i] = texto[i].lower() # Mantendo o texto em letra minúscula
     texto[i] = texto[i].replace(j,'') # Retirando todos os sinais
     texto[i] = ''.join([i for i in texto[i] if not i.isdigit()]) # Retirando os dígitos
+    
+nltk.download() # Realizando o download do pacote stopwords
+
+texto2 = ''.join(texto).split() # Unindo as frases e separando por palavras
+
+stopwordsnltk = nltk.corpus.stopwords.words('portuguese') # Extraindo as Stopwords do NLTK
+texto3 = [palavra for palavra in texto2 if palavra not in stopwordsnltk] # Retirando as Stopwords
 
 # Construindo o Dicionário:
-
-texto2 = ''.join(texto).split() # Unindo o conteúdo da lista e separando por palavras
 contaPalavra = {}
 
-# Contando as palavras
-for i in texto2:
-  contaPalavra[i] = texto2.count(i)
-
-print(contaPalavra)
+for i in texto3:
+  contaPalavra[i] = texto3.count(i) # Contando as palavras
 
 # Visualização Gráfica
 
 pd.set_option('display.max_rows', None) # Para visualizar todas as linhas do Dataframe
 df = pd.DataFrame(list(contaPalavra.items()), columns=['Palavra', 'Quantidade']) # Criando um Dataframe
 df = df.sort_values(by='Quantidade', ascending=False, ignore_index=True) # Ordenando de forma decrescente o Dataframe pela coluna Quantidade 
-df = df[df['Quantidade']>50] # Filtrando a coluna Quantidade para valores maiores que 50
+df = df[df['Quantidade']>26] # Filtrando a coluna Quantidade para valores maiores que 26
 
 sns.set_theme(style='darkgrid') # Definindo a cor de fundo
 plt.figure(figsize=(24,10)) # Definindo o tamanho do Dataframe
